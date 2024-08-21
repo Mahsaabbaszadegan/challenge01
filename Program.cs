5c67bfbb8315c4284cbbb7d672eff72021104921
Mahsaabbaszadegan <158215960+Mahsaabbaszadegan@users.noreply.github.com>
2024-08-21 13:38:40 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,6 +9,9 @@ namespace challenge01
     internal class Program
     {
         static void Main(string[] args)
+{
+   for (int i=0; i<10; i++) { 
+    Console.WriteLine("challenge01");
         {
         }
     }
